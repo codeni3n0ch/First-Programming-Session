fe6f9f3608c1210b0d301cfe9631111d2227105f
codeni3n0ch <eaf6155@students.uc-bcf.edu.ph>
2024-09-07 10:02:35 +0800

    Add files via upload

---
+++ Sales Invoice.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author m303user
+ */
+public class CC2 {
+    public static void main(String[]args){
+        // Store Information
+        System.out.println("Store Name:\"Enoch's Convinience Store\"");
+        System.out.println("Address: Calomboyan San Quintin Pangasinan");
+                
+        System.out.println("\nItem List");
+        // Item List
+        System.out.println("\nItems\t\tQuantity\tPrice");
+        System.out.println("EGG\t\t5\t\t40");
+        System.out.println("Corned Beef\t2\t\t50");
+        System.out.println("Pancit CAnton\t3\t\t45");
+        System.out.println("Coke\t\t1\t\t12");
+        System.out.println("Candy\t\t10\t\t10");
+        // Total Amount
+        double totalAmount = (5 * 40) + (2 * 50) + (3 * 45) + (1 * 12) + (10 * 10)
+       System.out.println("\nThe Total Price is: "+("\t\t157");        
+    }
+    
+    
+}
