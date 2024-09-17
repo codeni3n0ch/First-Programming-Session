c0905855727b82af90817e7923b2089350548a86
codeni3n0ch <eaf6155@students.uc-bcf.edu.ph>
2024-09-17 09:53:11 +0800

    Sales Invoice.java

--- Sales Invoice.java
+++ Sales Invoice.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author m303user
- */
 public class CC2 {
     public static void main(String[]args){
         // Store Information
