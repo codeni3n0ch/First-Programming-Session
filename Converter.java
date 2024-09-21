72df845fa99df5ac608487e37c50ee59a6048694
codeni3n0ch <eaf6155@students.uc-bcf.edu.ph>
2024-09-21 09:36:30 +0800

    Converter.java

--- Converter.java
+++ Converter.java
@@ -1,24 +1,28 @@
 import java.util.Scanner; //Import Scanner class
-public class Converter {
 
-    public static void main(String[] args){
+public class Converter {
+      public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        
+            // Ask for user input
          while (true) {
             System.out.print("Enter a decimal number or Enter 'STOP' to exit: ");
             String userInput = scanner.nextLine();
-            
+             // Convert decimal to binary
             try {
                 int decimalNumber = Integer.parseInt(userInput);
                 String binaryNumber = Integer.toBinaryString(decimalNumber);
-                System.out.println("Decimal: " + decimalNumber + " => Binary: " + binaryNumber);
+                System.out.println("Decimal: " + decimalNumber + " = Binary: " + binaryNumber);
             } catch (NumberFormatException e) {
-                 System.out.println("\t\t\tFinish");
+                 System.out.println("\t\tFinish");
+                 System.out.println("\tProgram Terminate");
                 return;
                    
-    }
+         }
          
-}
+      }
+   }
+     
 }
     
-}
+
+
