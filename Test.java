4718e95d912742098ead62bfde0a441752eadc12
codeni3n0ch <eaf6155@students.uc-bcf.edu.ph>
2024-09-21 09:47:20 +0800

    Test.java

--- Test.java
+++ Test.java
@@ -1,16 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author m303user
- */
-/*Anna
-September 6, 2024
-First Programming Session*/
 public class Test {
     public static void main(String[]args){
         System.out.print("Ma'am Anna \t\t Rhodora \n\n Quitaleg");
