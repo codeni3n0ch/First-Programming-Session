d7f08f2fd3272a58d67794d7d5f29daff9d58d1d
codeni3n0ch <eaf6155@students.uc-bcf.edu.ph>
2024-10-04 22:22:47 +0800

    DecimalConverter.java

--- DecimalConverter.java
+++ DecimalConverter.java
@@ -18,8 +18,17 @@ public class DecimalConverter {
                 String octal = Integer.toOctalString(decimal);
                 String hexa = Integer.toHexString(decimal);
 
+                // Separate the nibbles in the binary representation
+                String binaryNibbles = "";
+                for (int i = 0; i < binary.length(); i++) {
+                    if ((i % 4) == 0 && i != 0) {
+                        binaryNibbles = " " + binaryNibbles;
+                    }
+                    binaryNibbles = binary.charAt(i) + binaryNibbles;
+                }
+
                 System.out.println("Decimal: " + decimal);
-                System.out.println("Binary: " + binary);
+                System.out.println("Binary: " + binaryNibbles);
                 System.out.println("Octal: " + octal);
                 System.out.println("Hexadecimal: " + hexa);
                 System.out.println();
